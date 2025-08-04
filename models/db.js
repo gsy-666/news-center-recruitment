a31fd3c37ce5f23a2836712d6282da2f59c5bc51
gsy-666 <2065216811@qq.com>
2025-08-04 11:07:50 +0800

    修复数据库连接问题，更新配置文件

--- models/db.js
+++ models/db.js
@@ -1,22 +1,22 @@
 const { MongoClient } = require('mongodb');
 
-// 连接字符串
-const uri = 'mongodb://localhost:27017';
-const client = new MongoClient(uri);
+// 直接从环境变量读取，不要硬编码
+const MONGODB_URI = process.env.MONGODB_URI;
 
-// 数据库名称
-const dbName = 'newsCenterDB';
+if (!MONGODB_URI) {
+  throw new Error('请在 .env 文件中设置 MONGODB_URI 环境变量');
+}
 
-// 连接数据库
+// 连接数据库的函数
 async function connectDB() {
   try {
-    await client.connect();
-    console.log('成功连接到MongoDB数据库');
-    return client.db(dbName);
-  } catch (err) {
-    console.error('数据库连接失败:', err);
-    throw err;
+    const client = await MongoClient.connect(MONGODB_URI);
+    console.log('成功连接到 MongoDB Atlas');
+    return client.db(); // 返回数据库实例
+  } catch (error) {
+    console.error('数据库连接失败:', error);
+    throw error; // 让错误冒泡，方便日志排查
   }
 }
 
-module.exports = { connectDB, client };
+module.exports = connectDB;
